3a259a1383e433c0b4bc8c94642f56ddf9ffd34a
TeaDrinker <81148886+TeaDrinker12@users.noreply.github.com>
2021-03-29 15:46:16 +0300

    move stylesheet

--- shared/header.php
+++ shared/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <title><?= $PAGETITLE ?></title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="shared/style.css">
   </head>
   <body>
     <div id="wrapper">
